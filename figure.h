42b20e318707c8b0e6907e130b7499433bccfb15
Mark Albrand <mark.albrand5@gmail.com>
2024-08-26 20:10:28 -0600

    DVD
    Co-Authored-By: Javier Alejandro Ramírez Heredia <78290724+XJ4v1erX@users.noreply.github.com>
    Co-Authored-By: Jimena Hernández <77686378+Kojimena@users.noreply.github.com>

--- figure.h
+++ figure.h
@@ -1,19 +1,40 @@
+#pragma once
+
 #include <stdio.h>
 #include <SDL.h>
+#include <SDL_image.h>
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH 980
+#define SCREEN_HEIGHT 640
 
 // Función para inicializar una figura
 typedef struct {
     int x, y;           // Posición de la figura
     int width, height;  // Dimensiones de la figura
     int speedX, speedY; // Velocidad en los ejes X e Y
-    Uint32 color;       // Color de la figura
+    SDL_Color color;       // Color de la figura
+    SDL_Texture *texture;
 } Figura;
 
+SDL_Texture* loadTexture(const char* filename, SDL_Renderer* renderer) {
+    SDL_Surface* surface = IMG_Load(filename);
+    if (surface == NULL) {
+        printf("Error al cargar la imagen %s: %s\n", filename, IMG_GetError());
+        return NULL;
+    }
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    if (texture == NULL) {
+        printf("Error al crear la textura para %s: %s\n", filename, SDL_GetError());
+    } else {
+        printf("Textura cargada correctamente: %s\n", filename);
+    }
+    return texture;
+}
 
-Figura createFigura(int x, int y, int width, int height, int speedX, int speedY, Uint32 color) {
+
+
+Figura createFigura(int x, int y, int width, int height, int speedX, int speedY, const char* image, SDL_Renderer* renderer, SDL_Color color) {
     Figura figura;
     figura.x = x;
     figura.y = y;
@@ -21,7 +42,13 @@ Figura createFigura(int x, int y, int width, int height, int speedX, int speedY,
     figura.height = height;
     figura.speedX = speedX;
     figura.speedY = speedY;
+    figura.texture = loadTexture(image, renderer);
     figura.color = color;
+
+    if (figura.texture == NULL) {
+        printf("No se pudo cargar la figura en (%d, %d)\n", x, y);
+    }
+
     return figura;
 }
 
@@ -40,17 +67,18 @@ void moveFigura(Figura* figura) {
 }
 
 // Función para dibujar una figura en la superficie dada
-void drawFigura(SDL_Surface* surface, Figura* figura) {
+void drawFigura(SDL_Renderer* renderer, Figura* figura) {
     SDL_Rect rect = {figura->x, figura->y, figura->width, figura->height};
-    SDL_FillRect(surface, &rect, figura->color);
+    SDL_SetTextureColorMod(figura->texture, figura->color.r, figura->color.g, figura->color.b);
+    SDL_RenderCopy(renderer, figura->texture, NULL, &rect);
 }
 
-// Función para detectar colisión entre dos figuras
-// Función para detectar colisión entre dos figuras y manejar la respuesta
 // Función para detectar colisión entre dos figuras
 int checkCollision(Figura* a, Figura* b) {
     int collisionX = a->x < b->x + b->width && a->x + a->width > b->x;
     int collisionY = a->y < b->y + b->height && a->y + a->height > b->y;
     return collisionX && collisionY;
 }
+
+
 
--- main.c
+++ main.c
@@ -1,47 +1,33 @@
 #include <stdio.h>
 #include <SDL.h>
-
+#include <SDL_image.h>
 #include "figure.h"
-#include "color.h"
-
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH 980
+#define SCREEN_HEIGHT 640
 #define FPS 60
 
-#define N 2
+#define N 5
 
 Figura figuras[N];
 
 // inicializar figuras
-void initFiguras(SDL_Surface *screenSurface) {
+void initFiguras(SDL_Renderer *renderer) {
     for (int i = 0; i < N; i++) {
-        // elegir un color aleatorio
-        int c = rand() % 3;
-
         int x = rand() % (SCREEN_WIDTH - 50);  // [0, SCREEN_WIDTH - 50]
         int y = rand() % (SCREEN_HEIGHT - 50);  // [0, SCREEN_HEIGHT - 50]
 
         int spd_x = rand() % 10 - 5;  // [-5, 5]
         int spd_y = rand() % 10 - 5;  // [-5, 5]
 
-        switch (c) {
-            case 0:
-                figuras[i] = createFigura(x, y, 50, 50, spd_x, spd_y, RED(screenSurface));
-                break;
-            case 1:
-                figuras[i] = createFigura(x, y, 50, 50, spd_x, spd_y, BLUE(screenSurface));
-                break;
-            case 2:
-                figuras[i] = createFigura(x, y, 50, 50, spd_x, spd_y, GREEN(screenSurface));
-                break;
+        char* image = "assets/dvd_logo.png";
 
-            default:
-                figuras[i] = createFigura(x, y, 50, 50, spd_x, spd_y, RED(screenSurface));
-                break;
+        SDL_Color color = {rand() % 256, rand() % 256, rand() % 256, 255};
 
-        }
+        figuras[i] = createFigura(x, y, 64, 38, spd_x, spd_y, image, renderer, color);
     }
 }
+
+
 
 // Función para inicializar SDL y crear una ventana
 SDL_Window* initializeWindow(const char* title, int width, int height) {
@@ -54,10 +40,13 @@ SDL_Window* initializeWindow(const char* title, int width, int height) {
     if (window == NULL) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_Quit();
+        return NULL;
     }
 
     return window;
 }
+
+//
 
 // Función para cerrar SDL y destruir la ventana
 void cleanup(SDL_Window* window) {
@@ -66,24 +55,51 @@ void cleanup(SDL_Window* window) {
 }
 
 int main(int argc, char *argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    if (IMG_Init(IMG_INIT_PNG) == 0) {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
     SDL_Window* window = initializeWindow("Screen Saver", SCREEN_WIDTH, SCREEN_HEIGHT);
     if (window == NULL) {
         return 1;
     }
 
-    SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
 
     // Inicializar figuras
-    initFiguras(screenSurface);
+    initFiguras(renderer);
+
+    Uint32 startTicks, endTicks;
+    float fps = 0.0f;
+    char title[100];
 
     int running = 1;
     while (running) {
+        startTicks = SDL_GetTicks();
+
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 running = 0;
             }
         }
+
+        SDL_RenderClear(renderer);
 
         // Mover figuras
         for (int i = 0; i < N; i++) {
@@ -102,17 +118,18 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        // Borrar la pantalla
-        SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
-
         // Dibujar las figuras
-
         for (int i = 0; i < N; i++) {
-            drawFigura(screenSurface, &figuras[i]);
+            drawFigura(renderer, &figuras[i]);
         }
 
-        SDL_UpdateWindowSurface(window);
+        SDL_RenderPresent(renderer);
         SDL_Delay(1000 / FPS);
+
+        endTicks = SDL_GetTicks();
+        fps = 1000.0f / (endTicks - startTicks);
+        snprintf(title, sizeof(title), "Screen Saver - FPS: %.2f", fps);
+        SDL_SetWindowTitle(window, title);
     }
 
     cleanup(window);
